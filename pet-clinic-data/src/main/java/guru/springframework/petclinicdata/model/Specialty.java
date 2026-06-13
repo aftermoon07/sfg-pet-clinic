f773e5baea63567debefa2b87f9db865182009f9
Aditya Suryawanshi <adityasuryawanshi234@gmail.com>
2026-06-13 11:54:49 +0530

    Updated vet to use JPA. closes #37

--- pet-clinic-data/src/main/java/guru/springframework/petclinicdata/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinicdata/model/Specialty.java
@@ -1,6 +1,14 @@
 package guru.springframework.petclinicdata.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
+    @Column(name = "description")
     private String Description;
     public String getDescription() {
         return Description;
