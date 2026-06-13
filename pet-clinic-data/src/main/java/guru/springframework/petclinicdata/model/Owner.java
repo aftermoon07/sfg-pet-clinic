a16445af6154e1c44e024ab0d48d758102cd6c04
Aditya Suryawanshi <adityasuryawanshi234@gmail.com>
2026-06-13 11:47:02 +0530

    Updated owner with JPA entity mapping closes #36

--- pet-clinic-data/src/main/java/guru/springframework/petclinicdata/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinicdata/model/Owner.java
@@ -1,14 +1,23 @@
 package guru.springframework.petclinicdata.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name="owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
+    @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "owner")
     // 💡 FIXED: Initializing the Set directly avoids NullPointerExceptions when calling .add()
     private Set<Pet> pets = new HashSet<>();
 
